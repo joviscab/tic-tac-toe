3ac86083f09423cbf0b160c5143ba6cfc0787b76
joviscab <152897880+joviscab@users.noreply.github.com>
2024-06-15 17:27:13 +0200

    add gameBoard object and board logic

---
+++ script.js
@@ -0,0 +1,18 @@
+//gameBoard object
+function gameBoard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [
+        0, 1, 3,
+        4, 5, 6,
+        7, 8, 9
+    ];
+
+}
+
+//players object
+
+
+
+
+//game object
