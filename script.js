9fc9ec8d1c80c155a4ecf4cdb009bb6f0c9fc3ae
joviscab <152897880+joviscab@users.noreply.github.com>
2024-06-15 21:03:16 +0200

    change gameBoard obect defining with methods

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 //gameBoard object
-function gameBoard() {
-    const rows = 3;
-    const columns = 3;
-    const board = [
+const gameBoard = {
+    rows: 3,
+    columns: 3,
+    board: [
         0, 1, 3,
         4, 5, 6,
         7, 8, 9
-    ];
-    console.log(board);
-}
+    ],
+    playerOneMark: "x",
+    playerTwoMark: "o",
+
+    //print the board
+    printBoard() {
+        console.log(this.board.slice(0, this.columns).join(" "));
+        console.log(this.board.slice(this.columns, this.columns * 2).join(" "));
+        console.log(this.board.slice(this.columns * 2).join(" "));
+    },
+
+    //player square selection
+    playerSelection(index, mark) {
+        if (index >= 0 && index < this.board.length && typeof this.board[index] === 'number') {
+            this.board[index] = mark;
+        } else {
+            console.log("Invalid move");
+        }
+    }
+};
+
+
+
 
 //players object
 function newPlayer (name) {
@@ -32,12 +52,10 @@ playerOne.addScore();
 playerOne.addScore();
 playerTwo.addScore();
 
-//const john = newPlayer("john");
-//john.addScore();
-//john.getScore();
 console.log({playerOneName: playerOne.playerOneName, score: playerOne.getScore()});
 console.log({playerTwoName: playerTwo.playerTwoName, score: playerTwo.getScore()});
 
-
-
 //game object
+function playRound () {
+
+}
