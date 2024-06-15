37176866c684498c296fb7814ec752e337d2f07b
joviscab <152897880+joviscab@users.noreply.github.com>
2024-06-15 20:23:23 +0200

    add newPlayer function

--- script.js
+++ script.js
@@ -7,11 +7,36 @@ function gameBoard() {
         4, 5, 6,
         7, 8, 9
     ];
-
+    console.log(board);
 }
 
 //players object
+function newPlayer (name) {
+    const playerOneName = name;
+    const playerTwoName = name;
 
+    let score = 0;
+    const getScore = () => score;
+    const addScore = () => score++;
+
+    return {playerOneName, playerTwoName, getScore, addScore};
+}
+
+const playerOneName = prompt("Enter player 1 name:");
+const playerTwoName = prompt("Enter player 2 name:");
+
+const playerOne = newPlayer(playerOneName);
+const playerTwo = newPlayer(playerTwoName);
+
+playerOne.addScore();
+playerOne.addScore();
+playerTwo.addScore();
+
+//const john = newPlayer("john");
+//john.addScore();
+//john.getScore();
+console.log({playerOneName: playerOne.playerOneName, score: playerOne.getScore()});
+console.log({playerTwoName: playerTwo.playerTwoName, score: playerTwo.getScore()});
 
 
 
